87bc60009eeff7b32bb14058e6b40d553d9e191e
Toriola <faaedah@gmail.com>
2024-03-09 23:24:40 +0100

    ADD: added placeholder forr name & email

--- src/components/ProfileData.jsx
+++ src/components/ProfileData.jsx
@@ -5,26 +5,39 @@ function ProfileData({ textStyleClass }) {
 
    let firstName = userData?.firstName;
    let lastName = userData?.lastName;
-   let initials = firstName?.charAt(0) + "." + lastName?.charAt(0);
+   let initials =
+      firstName && lastName
+         ? firstName.charAt(0) + "." + lastName.charAt(0)
+         : "";
 
    return (
       <>
          {!userData?.photo ? (
-            <div className="initials photo">{initials}</div>
+            <div className="initials photo bg-grey-2">{initials}</div>
          ) : (
             <img src={userData?.photo} alt="" className="photo" />
          )}
-         <p
-            className={`${textStyleClass} font-bold text-[32px] text-dark-grey mt-6 mb-2 text-center capitalize`}
-         >
-            {firstName} {lastName}
-         </p>
-         <a
-            href={`mailto: ${userData?.profileEmail}`}
-            className="text-grey-1 text-center block"
-         >
-            {userData?.profileEmail}
-         </a>
+
+         {userData.firstName ? (
+            <>
+               <p
+                  className={`${textStyleClass} font-bold text-[32px] text-dark-grey mt-6 mb-2 text-center capitalize`}
+               >
+                  {firstName} {lastName}
+               </p>
+               <a
+                  href={`mailto: ${userData?.profileEmail}`}
+                  className="text-grey-1 text-center block"
+               >
+                  {userData?.profileEmail}
+               </a>
+            </>
+         ) : (
+            <div>
+               <span className="bg-grey-2 rounded-full h-4 w-36 flex mx-auto mt-6" />
+               <span className="bg-grey-2 rounded-full h-2 w-20 flex mx-auto mt-4" />
+            </div>
+         )}
       </>
    );
 }
